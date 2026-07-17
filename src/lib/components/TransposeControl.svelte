9dc0ddf421fd5120b3c7437a9460053ff4f8e86f
Anton Sakhanovych <dev@antonsakhanovych.com>
2026-07-17 19:45:24 -0400

    Add transpose control and optional header image to song view

---
+++ src/lib/components/TransposeControl.svelte
@@ -0,0 +1,10 @@
+<!-- src/lib/components/TransposeControl.svelte -->
+<script lang="ts">
+  let { semitones, onChange }: { semitones: number; onChange: (n: number) => void } = $props();
+</script>
+
+<div class="transpose">
+  <button onclick={() => onChange(Math.max(semitones - 1, -6))} aria-label="Transpose down">-</button>
+  <span>{semitones > 0 ? `+${semitones}` : semitones}</span>
+  <button onclick={() => onChange(Math.min(semitones + 1, 6))} aria-label="Transpose up">+</button>
+</div>
--- src/routes/songs/[slug]/+page.svelte
+++ src/routes/songs/[slug]/+page.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
   import Song from '$lib/components/Song.svelte';
+  import TransposeControl from '$lib/components/TransposeControl.svelte';
 
   let { data } = $props();
+  let semitones = $state(0);
+  const transposedSong = $derived(data.song.transpose(semitones));
 </script>
+
+{#if data.imageFilename}
+  <img src={`/songs/${data.imageFilename}`} alt="" class="header-image" />
+{/if}
 
 <h1>{data.song.title}</h1>
 {#if data.song.artist}
   <p class="artist">{data.song.artist}</p>
 {/if}
 
-<Song song={data.song} />
+<TransposeControl {semitones} onChange={(n) => (semitones = n)} />
+
+<Song song={transposedSong} />
