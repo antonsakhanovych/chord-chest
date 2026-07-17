315f56a340b5364ffd7d2c6d27f7b0dc80974566
Anton Sakhanovych <dev@antonsakhanovych.com>
2026-07-17 19:23:50 -0400

    Add song list page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  let { data } = $props();
+</script>
+
+<h1>Chord Chest</h1>
+
+<ul class="song-list">
+  {#each data.songs as song (song.slug)}
+    <li>
+      <a href={`/songs/${song.slug}`}>
+        <span class="title">{song.title}</span>
+        {#if song.artist}
+          <span class="artist">{song.artist}</span>
+        {/if}
+      </a>
+    </li>
+  {/each}
+</ul>
---
+++ src/routes/+page.ts
@@ -0,0 +1,6 @@
+import { getSongFiles } from '$lib/songs/loadAll';
+import { buildIndex } from '$lib/songs/songIndex';
+
+export function load() {
+  return { songs: buildIndex(getSongFiles()) };
+}
